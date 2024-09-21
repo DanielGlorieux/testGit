b7d32ef3fc6a9ceb3f56886855af280e0c76a075
DanielGlorieux <danielglorieuxilboudo@gmail.com>
2024-09-21 14:33:04 +0000

    Premiere sauvegarde

---
+++ main.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(){
+    int a;
+    printf("Entrer un nombre: \n");
+    scanf("%d",&a);
+    printf("\nVous avez entre le chiffre %d ",a);
+    return 0;
+}
